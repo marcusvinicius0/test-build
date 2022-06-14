07c669d100ff58466ec832ef6fe9d167f03597a5
Marcus Vinicius <begheli@gmail.com>
2022-06-14 20:55:55 -0300

    configura port heroku

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express, {Request, Response} from 'express';
+const cors = require('cors');
 import { router } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 
-const port = 3333;
+
 
 app.use(router);
 
-app.listen(port, () => console.log('server online'))
+app.listen(process.env.PORT || 3000, () => console.log('server online'))
 
